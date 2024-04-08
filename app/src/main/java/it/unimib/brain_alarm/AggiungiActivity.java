705df9e536b530e360bebe589f74d1b57c75b4fd
sarar <s.rizzi22@campus.unimib.it>
2024-04-08 12:30:36 +0200

    sara

--- app/src/main/java/it/unimib/brain_alarm/AggiungiActivity.java
+++ app/src/main/java/it/unimib/brain_alarm/AggiungiActivity.java
@@ -2,27 +2,14 @@ package it.unimib.brain_alarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TimePicker;
-import android.widget.Toast;
-import android.widget.ToggleButton;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Calendar;
-
-import android.os.Bundle;
 
 import com.google.android.material.textfield.TextInputLayout;
 
-public class WelcomeActivity extends AppCompatActivity {
-    private static final String TAG = WelcomeActivity.class.getSimpleName();
+public class AggiungiActivity extends AppCompatActivity {
+    private static final String TAG = AggiungiActivity.class.getSimpleName();
 
     private TextInputLayout textInput;
 
@@ -31,9 +18,10 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);  //da qua cambio l'avvio del programma
+        setContentView(R.layout.activity_aggiungi);  //da qua cambio l'avvio del programma
 
         final Button buttonConferma = findViewById(R.id.conferma);
+
         textInput = findViewById(R.id.nomeSveglia);
 
         buttonConferma.setOnClickListener(v -> {
---
+++ app/src/main/java/it/unimib/brain_alarm/HomeActivity.java
@@ -0,0 +1,44 @@
+package it.unimib.brain_alarm;
+
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+    private static final String TAG = AggiungiActivity.class.getSimpleName();
+
+    private TextInputLayout textInput;
+
+    private TextInputLayout textInputLayoutEmail;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        final Button buttonAggiungi = findViewById(R.id.bottoneAggiungiSveglia);
+
+         buttonAggiungi.setOnClickListener(v -> {
+            startActivityBasedOnCondition(AggiungiActivity.class,
+            R.id.navigate_to_newsPreferencesActivity);
+
+        }
+    });
+
+    private void startActivityBasedOnCondition(Class<?> destinationActivity, int destination){
+        if(USE_NAVIGATION_COMPONENT){
+            Navigation.findNavController(requireView()).navigate(destination);
+        }else{
+            Intent intent=new Intent(requireContext(),destinationActivity);
+            startActivity(intent);
+        }
+        requireActivity().finish();
+        }
+
+}

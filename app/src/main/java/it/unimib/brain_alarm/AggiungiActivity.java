a7d9b0b6cf2bd6133475a446119a45a0999b76a3
Mirko <m.giussani26@campus.unimib.it>
2024-04-19 09:31:12 +0200

    base di partenza con le modifiche di Sara

--- app/src/main/java/it/unimib/brain_alarm/AggiungiActivity.java
+++ app/src/main/java/it/unimib/brain_alarm/AggiungiActivity.java
@@ -11,25 +11,11 @@ import com.google.android.material.textfield.TextInputLayout;
 public class AggiungiActivity extends AppCompatActivity {
     private static final String TAG = AggiungiActivity.class.getSimpleName();
 
-    private TextInputLayout textInput;
-
-
     private TextInputLayout textInputLayoutEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aggiungi);  //da qua cambio l'avvio del programma
 
-        final Button buttonConferma = findViewById(R.id.conferma);
-
-        textInput = findViewById(R.id.nomeSveglia);
-
-        buttonConferma.setOnClickListener(v -> {
-
-            String nomeSveglia = textInput.getEditText().getText().toString();
-
-            // messaggio TAG
-            Log.d(TAG, "nome sveglia: " + nomeSveglia);
-        });
     }
 }

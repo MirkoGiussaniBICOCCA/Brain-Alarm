4146a0746a415615c480d910bd2c9d1419b150c3
sarar <s.rizzi22@campus.unimib.it>
2024-04-19 14:24:13 +0200

    aggiunto file json e recycler view. manca aggiungere file news.

--- app/src/main/java/it/unimib/brain_alarm/riposoFragment.java
+++ app/src/main/java/it/unimib/brain_alarm/riposoFragment.java
@@ -2,17 +2,21 @@ package it.unimib.brain_alarm;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link riposoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
+import it.unimib.worldnews.adapter.NewsRecyclerViewAdapter;
+
+
+
 public class riposoFragment extends Fragment {
 
 
@@ -37,4 +41,40 @@ public class riposoFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_riposo, container, false);
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+        RecyclerView recyclerViewCountryNews = view.findViewById(R.id.recyclerview_country_news);
+        RecyclerView.LayoutManager layoutManager =
+                new LinearLayoutManager(requireContext(),
+                        LinearLayoutManager.VERTICAL, false);
+
+        List<News> newsList = getNewsListWithWithGSon();
+
+        NewsRecyclerViewAdapter newsRecyclerViewAdapter = new NewsRecyclerViewAdapter(newsList,
+                new NewsRecyclerViewAdapter.OnItemClickListener() {
+                    @Override
+                    public void onNewsItemClick(News news) {
+                        Snackbar.make(view, news.getTitle(), Snackbar.LENGTH_SHORT).show();
+                    }
+
+                    //clicco sulla cella esce snackbar titolo, clicco su cancella esce dimensione lista
+                    @Override
+                    public void onDeleteButtonPressed(int position) {
+                        Snackbar.make(view, getString(R.string.list_size_message) + newsList.size(),
+                                Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+        recyclerViewCountryNews.setLayoutManager(layoutManager);
+        recyclerViewCountryNews.setAdapter(newsRecyclerViewAdapter);
+
+
+
+    }
+
 }

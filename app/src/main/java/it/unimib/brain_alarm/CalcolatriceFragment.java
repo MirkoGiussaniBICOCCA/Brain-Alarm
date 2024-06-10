b702b34e0567781e2a0f4553580f03dee21be553
sarar <s.rizzi22@campus.unimib.it>
2024-06-10 17:35:05 +0200

    sara. memory funzionante. va messo il suono in base a cosa si seleziona e fare vibrazione e suono se non si ha app aperta

--- app/src/main/java/it/unimib/brain_alarm/CalcolatriceFragment.java
+++ app/src/main/java/it/unimib/brain_alarm/CalcolatriceFragment.java
@@ -11,17 +11,31 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Random;
+
+import it.unimib.brain_alarm.News.Result;
 
 
 public class CalcolatriceFragment extends Fragment {
 
-    TextView ripcalc;
+    TextView textQuestion;
+    EditText editTextAnswer;
+    Button buttonSubmit;
+
+    int currentQuestion = 0;
+    int rightAnswer = 0;
+    final int TOTAL_QUESTIONS = 3;
+    int remainingRepetitions;
 
     public CalcolatriceFragment() {
         // Required empty public constructor
     }
-
 
     public static CalcolatriceFragment newInstance() {
         CalcolatriceFragment fragment = new CalcolatriceFragment();
@@ -40,21 +54,71 @@ public class CalcolatriceFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_calcolatrice, container, false);
     }
 
-   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         String ripMissioni = CalcolatriceFragmentArgs.fromBundle(getArguments()).getRipMissioni();
+        remainingRepetitions = ripMissioni.charAt(0) - '0';
+
+        if (remainingRepetitions != 0) {
+
+            textQuestion = view.findViewById(R.id.textQuestion);
+            editTextAnswer = view.findViewById(R.id.editTextAnswer);
+            buttonSubmit = view.findViewById(R.id.buttonSubmit);
+
+            getARandomQuestion();
+
+            buttonSubmit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String answerText = editTextAnswer.getText().toString();
+                    if (!answerText.isEmpty()) {
+                        int userAnswer = Integer.parseInt(answerText);
+                        if (userAnswer == rightAnswer) {
+                            getARandomQuestion();
+                            editTextAnswer.setText("");
+                            Snackbar.make(view, "Corretto", Snackbar.LENGTH_SHORT).show();
+                            remainingRepetitions--;
+                            if (remainingRepetitions > 0) {
+                                Snackbar.make(view, "Ripetizione completata! Ancora " + remainingRepetitions + " ripetizioni.", Snackbar.LENGTH_SHORT).show();
+                            } else {
+                                Snackbar.make(view, "Hai completato tutte le ripetizioni!", Snackbar.LENGTH_SHORT).show();
+                                getActivity().finish();
+                            }
+                        } else {
+                            Snackbar.make(view, "Risposta sbagliata, riprova", Snackbar.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Snackbar.make(view, "Per favore, inserisci una risposta", Snackbar.LENGTH_SHORT).show();
+                    }
+                }
+            });
 
 
 
-        if(ripMissioni.charAt(0)!='0') {
-            ripcalc = view.findViewById(R.id.textRipetizioni);
-            ripcalc.setText(ripMissioni);
+        } else {
+            CalcolatriceFragmentDirections.ActionCalcolatriceFragmentToMemoryFragment action = CalcolatriceFragmentDirections.actionCalcolatriceFragmentToMemoryFragment(ripMissioni);
+            Navigation.findNavController(view).navigate(action);
         }
-        else {
-           CalcolatriceFragmentDirections.ActionCalcolatriceFragmentToMemoryFragment action = CalcolatriceFragmentDirections.actionCalcolatriceFragmentToMemoryFragment(ripMissioni);
-           Navigation.findNavController(view).navigate(action);  }
     }
 
+    private void getARandomQuestion() {
+        int firstNumber = new Random().nextInt(100);
+        int secondNumber = new Random().nextInt(100);
+        int operation = new Random().nextInt(2) + 1;
+
+        if (operation == 1) {
+            rightAnswer = firstNumber + secondNumber;
+            textQuestion.setText(firstNumber + " + " + secondNumber + " = ?");
+        } else if (operation == 2) {
+            if (firstNumber < secondNumber) {
+                rightAnswer = secondNumber - firstNumber;
+                textQuestion.setText(secondNumber + " - " + firstNumber + " = ?");
+            } else {
+                rightAnswer = firstNumber - secondNumber;
+                textQuestion.setText(firstNumber + " - " + secondNumber + " = ?");
+            }
+        }
+    }
 }
